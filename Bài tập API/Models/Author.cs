8f53a32d8bb929a4d7794fece3ae34ed63084087
nguyenthangviet2 <vietthang52004@gmail.com>
2024-03-29 22:24:55 +0700

    project API

---
+++ Bài tập API/Models/Author.cs
@@ -0,0 +1,16 @@
+﻿using Bài_tập_API.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bài_tập_API.Models
+{
+    public class Author
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        // One-to-many relationship with books
+        public List<Book>? Books { get; set; }
+    }
+}
